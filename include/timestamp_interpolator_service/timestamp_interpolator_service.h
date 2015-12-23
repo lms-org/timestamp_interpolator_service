e5fab533f71b0a845895a247c5d94792ec28d308
Markus Herb <mail@markusherb.de>
2015-12-23 01:02:06 +0100

    Basic time interpolation

--- include/timestamp_interpolator_service/timestamp_interpolator_service.h
+++ include/timestamp_interpolator_service/timestamp_interpolator_service.h
@@ -12,6 +12,67 @@ class TimestampInterpolatorService : public lms::Service {
 public:
     bool init() override;
     void destroy() override;
+public:
+    //! Timestamp type
+    typedef lms::Time Timestamp;
+    //! Clock identifier type
+    typedef std::string Clock;
+
+protected:
+    //! Pair of clock identifiers as key for synchronization points container
+    typedef std::pair<Clock, Clock> ClockPair;
+    //! Pair of timestamps from two clocks
+    typedef std::pair<Timestamp, Timestamp> TimestampPair;
+    //! Start and End pair of clock sync points
+    typedef std::vector<TimestampPair> SyncPointList;
+    typedef std::map<ClockPair, SyncPointList> Container;
+
+public:
+    /**
+     * @brief Create synchronization point for two clocks
+     *
+     * @param aClock Identifier of the first clock
+     * @param bClock Identifier of the second clock
+     * @param aTime Synchronization timestamp of the first clock
+     * @param bTime Synchronization timestamp of the second clock
+     */
+    void sync(const Clock& aClock, const Clock& bClock, const Timestamp& aTime, const Timestamp& bTime );
+
+    /**
+     * @brief Interpolate timestamp from one clock to another clock
+     *
+     * @note If no sync points are available for the clock combination, the input timestamp is returned
+     *
+     * @param from The clock identifier to convert the timestmap from
+     * @param to The clock identifier to convert the timestamp to
+     * @param timestamp the clock identifier of the source clock to convert
+     * @return Interpolated timestamp of the target clock
+     */
+    Timestamp interpolate(const Clock& from, const Clock& to, const Timestamp& timestamp) const;
+
+    // TODO: drift calculation
+protected:
+
+    /**
+     * @brief Clock pair creation
+     *
+     * This implementation ensures that clock pairs (as map keys) are unique
+     */
+    inline static ClockPair makeClockPair(const Clock& a, const Clock& b, bool& switched)
+    {
+        switched = (a > b);
+        if(!switched)
+        {
+            return std::make_pair(a, b);
+        }
+        else
+        {
+            return std::make_pair(b, a);
+        }
+    }
+protected:
+    //! Container to store synchronization points
+    Container syncPoints;
 };
 
 } // namespace timestamp_interpolator_service
--- src/timestamp_interpolator_service.cpp
+++ src/timestamp_interpolator_service.cpp
@@ -1,5 +1,7 @@
 #include "timestamp_interpolator_service/timestamp_interpolator_service.h"
 
+#include <lms/math/interpolation.h>
+
 namespace timestamp_interpolator_service {
 
 bool TimestampInterpolatorService::init() {
@@ -9,4 +11,71 @@ bool TimestampInterpolatorService::init() {
 void TimestampInterpolatorService::destroy() {
 }
 
+void TimestampInterpolatorService::sync(const TimestampInterpolatorService::Clock &aClock,
+                                        const TimestampInterpolatorService::Clock &bClock,
+                                        const TimestampInterpolatorService::Timestamp &aTime,
+                                        const TimestampInterpolatorService::Timestamp &bTime)
+{
+    bool switched;
+    auto key = makeClockPair(aClock, bClock, switched);
+    auto sync = switched ? std::make_pair(bTime, aTime) : std::make_pair(aTime, bTime);
+    auto& vec = syncPoints[key];
+
+    if(vec.size() > 1)
+    {
+        // Remove last element
+        vec.pop_back();
+    }
+
+    // Insert new sync point at the end
+    vec.push_back(sync);
+}
+
+TimestampInterpolatorService::Timestamp TimestampInterpolatorService::interpolate(
+        const TimestampInterpolatorService::Clock &from, const TimestampInterpolatorService::Clock &to,
+        const TimestampInterpolatorService::Timestamp &timestamp) const
+{
+    bool switched;
+    auto key = makeClockPair(from, to, switched);
+
+    const auto it = syncPoints.find(key);
+    if(it == syncPoints.end())
+    {
+        // No sync point found
+        return timestamp;
+    }
+
+    const auto& vec = it->second;
+    if(vec.size() == 0)
+    {
+        // No sync points
+        return timestamp;
+    }
+
+    Timestamp fromStart, fromEnd, toStart, toEnd;
+    std::tie(fromStart, toStart) = vec.front();
+    std::tie(fromEnd, toEnd) = vec.back();
+
+    if(switched)
+    {
+        // Swap from and to fields
+        std::swap(fromStart, toStart);
+        std::swap(fromEnd, toEnd);
+    }
+
+    // Compute interpolation
+    Timestamp::TimeType result;
+    if(lms::math::linearInterpolation<Timestamp::TimeType>(fromStart.micros(), toStart.micros(),
+                                                           fromEnd.micros(), toEnd.micros(),
+                                                           timestamp.micros(), result))
+    {
+        return Timestamp::fromMicros(result);
+    }
+    else
+    {
+        // Start and End timestamp of source are identical -> no real interp possible
+        // => Just adjust clock offset
+        return toEnd + ( timestamp - fromEnd );
+    }
+}
 } // namespace timestamp_interpolator_service
