7a87152e91903a929b80e33e9ed61f532f0a86be
phibedy <philipp.schmette@gmx.de>
2016-10-21 13:35:55 +0200

    LMS Interface

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,3 +1,5 @@
 #include "timestamp_interpolator_service/timestamp_interpolator_service.h"
 
+LMS_EXPORT_BEGIN
 LMS_EXPORT_SERVICE_NS(timestamp_interpolator_service,TimestampInterpolatorService)
+LMS_EXPORT_END
